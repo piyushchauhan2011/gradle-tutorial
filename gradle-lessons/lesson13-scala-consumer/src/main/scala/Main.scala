4135f4bb2c2fcc212eb58301ce4db5743f9c3595
Piyush Chauhan <piyushchauhan2011@gmail.com>
2026-02-01 08:38:00 +0700

    Refactor Main.scala and remove outdated JavaFX UI tests
    - Change `lib` variable in Main.scala to private for better encapsulation.
    - Delete obsolete UI test files for CalculatorApp, HelloWorldApp, and TodoListApp as part of cleanup efforts.

--- gradle-lessons/lesson13-scala-consumer/src/main/scala/Main.scala
+++ gradle-lessons/lesson13-scala-consumer/src/main/scala/Main.scala
@@ -1,4 +1,4 @@
 object Main extends App {
-  val lib = new com.example.Library()
-  println(s"Library version: ${lib.getVersion()}")
+  private val lib = new com.example.Library()
+  println(s"Library version: ${lib.getVersion}")
 }
--- gradle-lessons/lesson18-javafx/src/test/java/com/example/javafx/TodoListAppUiTest.java
+++ gradle-lessons/lesson18-javafx/src/test/java/com/example/javafx/TodoListAppUiTest.java
@@ -1,7 +1,6 @@
 package com.example.javafx;
 
 import javafx.stage.Stage;
-import org.junit.Before;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.util.WaitForAsyncUtils;
